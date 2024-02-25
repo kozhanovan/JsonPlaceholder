e51ffe82d76dd0200c7c08de3028d38cce140a10
Andrey Kozhanv <kozhanovan@gmail.com>
2024-02-25 15:27:50 +0300

    Cleanup code

--- src/main/java/com/typicode/TheApp.java
+++ src/main/java/com/typicode/TheApp.java
@@ -3,12 +3,9 @@ package com.typicode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 public class TheApp {
     public static void main(String[] args) {
-        System.out.println("Args = " + Arrays.toString(args));
         SpringApplication.run(TheApp.class, args);
     }
 }
